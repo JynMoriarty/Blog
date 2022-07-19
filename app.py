4ff927092ecbe7c4643d76efdb318161128bc3e3
Marina Supiot <m.v.supiot@gmail.com>
2022-07-19 13:32:06 +0200

    recuperer les donnees

--- app.py
+++ app.py
@@ -1,14 +1,32 @@
 from flask import Flask, render_template, redirect, url_for, request, session
+from datetime import datetime
+from pymongo import MongoClient
 import remplirbdd
+client = MongoClient("127.0.0.1:27017")
 app = Flask(__name__)
 
 @app.route("/")
 def accueil():
-    return render_template("accueil.html")
+    liste_article = [
 
-@app.route()
-def article():
-    return render_template("article.html")
+        {
+            "titre": "Titre1",
+            "text": "Lorem ",
+            "date": str(datetime.now())
+        }, {
+            "titre": "Titre2",
+            "text": "Lorem2",
+            "date": str(datetime.now())
+        },{
+            "titre": "Titre3",
+            "text": "Lorem3",
+            "date": str(datetime.now())
+        }
+    ]
+    return render_template("accueil.html", articles = liste_article)
 
-@app route("/")
-def bdd():
+#@app.route()
+#def article():
+    #return render_template("article.html")
+#@app route("/")
+#def bdd():
