2deafb5b446358b9075b627e563d77b59a5989c7
Omar Sahboun <omar.lilian.sahboun@gmail.com>
2022-07-19 09:54:00 +0200

    Réunion avec Antoine

--- remplirbdd.py
+++ remplirbdd.py
@@ -2,18 +2,32 @@ from pymongo import MongoClient
 from datetime import datetime
 client = MongoClient("127.0.0.1:27017")
 db = client.blog
-article = db.articles
+articles = db.articles
 
 
-article.test.insert_one(
+article1 =(
 
     {   
         "titre" : "Chapitre 1 : Le commencement",
         "date":str(datetime.now()),
-        "pseudo" : "Michel",
         "texte":"Il était une fois , une guerre sanglante qui opposa les chevaliers du continent d'Enkidiev et les insectes du seigneur Amecareth",
-        "commentaire": "."
-    }   
+        "commentaire": [
+            {
+            "date" :str(datetime.now()),
+            "Username": None,
+            "User_ID": None,
+            "text": "lorem ipsum"
+        
+            }, {
+            "date" :str(datetime.now()),
+            "Username": None,
+            "User_ID": None,
+            "text": "lorem ipsum 2"
+        
+            }
+        ]   
+    }        
 
 )
-
+reponse = articles.insert_one(article1)
+print(articles.find_one({}))
