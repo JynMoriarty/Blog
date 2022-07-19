bb0921889379470e8fd93838b9554256226843ea
Jeffrey NGUYEN <jeffrey69140@gmail.com>
2022-07-19 09:15:31 +0200

    ajout du fichier base de donnee

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template, redirect, url_for, request, session
-
+import remplirbdd
 app = Flask(__name__)
 
 @app.route("/")
 def accueil():
     return render_template("accueil.html")
 
-@app.route('/article/<nom>')
-def article(nom):
-    return render_template("article.html", nom=nom)
+@app.route()
+def article():
+    return render_template("article.html")
+
+@app route("/")
+def bdd():
